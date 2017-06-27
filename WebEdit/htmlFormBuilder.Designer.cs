df0d551fccc61ea668d48c0b4c5bb6495d7ad3f4
rohit <rohitpai@hotmail.co.uk>
2017-06-27 19:20:38 +0100

    Updated Form Builder

---
+++ WebEdit/htmlFormBuilder.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace htmlEditor
+{
+    partial class htmlFormBuilder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textarea = new System.Windows.Forms.Label();
+            this.select = new System.Windows.Forms.Label();
+            this.radio = new System.Windows.Forms.Label();
+            this.check = new System.Windows.Forms.Label();
+            this.numberRange = new System.Windows.Forms.Label();
+            this.mobileNo = new System.Windows.Forms.Label();
+            this.inpAdress = new System.Windows.Forms.Label();
+            this.inpTxt = new System.Windows.Forms.Label();
+            this.inpEmail = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.rtb3 = new System.Windows.Forms.RichTextBox();
+            this.rtb2 = new System.Windows.Forms.RichTextBox();
+            this.rtb1 = new System.Windows.Forms.RichTextBox();
+            this.panel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.finish = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.finish);
+            this.panel1.Controls.Add(this.textarea);
+            this.panel1.Controls.Add(this.select);
+            this.panel1.Controls.Add(this.radio);
+            this.panel1.Controls.Add(this.check);
+            this.panel1.Controls.Add(this.numberRange);
+            this.panel1.Controls.Add(this.mobileNo);
+            this.panel1.Controls.Add(this.inpAdress);
+            this.panel1.Controls.Add(this.inpTxt);
+            this.panel1.Controls.Add(this.inpEmail);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(303, 555);
+            this.panel1.TabIndex = 0;
+            // 
+            // textarea
+            // 
+            this.textarea.AutoSize = true;
+            this.textarea.Location = new System.Drawing.Point(23, 301);
+            this.textarea.Name = "textarea";
+            this.textarea.Size = new System.Drawing.Size(107, 17);
+            this.textarea.TabIndex = 8;
+            this.textarea.Text = "Multiline textbox";
+            this.textarea.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textarea_MouseDown);
+            // 
+            // select
+            // 
+            this.select.AutoSize = true;
+            this.select.Location = new System.Drawing.Point(23, 269);
+            this.select.Name = "select";
+            this.select.Size = new System.Drawing.Size(104, 17);
+            this.select.TabIndex = 7;
+            this.select.Text = "Drop Down List";
+            this.select.MouseDown += new System.Windows.Forms.MouseEventHandler(this.select_MouseDown);
+            // 
+            // radio
+            // 
+            this.radio.AutoSize = true;
+            this.radio.Location = new System.Drawing.Point(23, 199);
+            this.radio.Name = "radio";
+            this.radio.Size = new System.Drawing.Size(80, 17);
+            this.radio.TabIndex = 6;
+            this.radio.Text = "Radio Input";
+            this.radio.MouseDown += new System.Windows.Forms.MouseEventHandler(this.radio_MouseDown);
+            // 
+            // check
+            // 
+            this.check.AutoSize = true;
+            this.check.Location = new System.Drawing.Point(23, 231);
+            this.check.Name = "check";
+            this.check.Size = new System.Drawing.Size(109, 17);
+            this.check.TabIndex = 5;
+            this.check.Text = "Check Box input";
+            this.check.MouseDown += new System.Windows.Forms.MouseEventHandler(this.check_MouseDown);
+            // 
+            // numberRange
+            // 
+            this.numberRange.AutoSize = true;
+            this.numberRange.Location = new System.Drawing.Point(23, 162);
+            this.numberRange.Name = "numberRange";
+            this.numberRange.Size = new System.Drawing.Size(104, 17);
+            this.numberRange.TabIndex = 4;
+            this.numberRange.Text = "Number Range";
+            this.numberRange.MouseDown += new System.Windows.Forms.MouseEventHandler(this.numberRange_MouseDown);
+            // 
+            // mobileNo
+            // 
+            this.mobileNo.AutoSize = true;
+            this.mobileNo.Location = new System.Drawing.Point(23, 128);
+            this.mobileNo.Name = "mobileNo";
+            this.mobileNo.Size = new System.Drawing.Size(58, 17);
+            this.mobileNo.TabIndex = 3;
+            this.mobileNo.Text = "Number";
+            this.mobileNo.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mobileNo_MouseDown);
+            // 
+            // inpAdress
+            // 
+            this.inpAdress.AutoSize = true;
+            this.inpAdress.Location = new System.Drawing.Point(23, 93);
+            this.inpAdress.Name = "inpAdress";
+            this.inpAdress.Size = new System.Drawing.Size(87, 17);
+            this.inpAdress.TabIndex = 2;
+            this.inpAdress.Text = "Input Adress";
+            this.inpAdress.MouseDown += new System.Windows.Forms.MouseEventHandler(this.inpAdress_MouseDown);
+            // 
+            // inpTxt
+            // 
+            this.inpTxt.AutoSize = true;
+            this.inpTxt.BackColor = System.Drawing.SystemColors.Control;
+            this.inpTxt.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.inpTxt.Location = new System.Drawing.Point(23, 21);
+            this.inpTxt.Name = "inpTxt";
+            this.inpTxt.Size = new System.Drawing.Size(70, 17);
+            this.inpTxt.TabIndex = 0;
+            this.inpTxt.Text = "Input Text";
+            this.inpTxt.MouseDown += new System.Windows.Forms.MouseEventHandler(this.inpTxt_MouseDown);
+            // 
+            // inpEmail
+            // 
+            this.inpEmail.AutoSize = true;
+            this.inpEmail.Location = new System.Drawing.Point(23, 56);
+            this.inpEmail.Name = "inpEmail";
+            this.inpEmail.Size = new System.Drawing.Size(82, 17);
+            this.inpEmail.TabIndex = 1;
+            this.inpEmail.Text = "Input E-mail";
+            this.inpEmail.DragEnter += new System.Windows.Forms.DragEventHandler(this.inpEmail_DragEnter);
+            this.inpEmail.MouseDown += new System.Windows.Forms.MouseEventHandler(this.inpEmail_MouseDown);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.rtb3);
+            this.panel3.Controls.Add(this.rtb2);
+            this.panel3.Controls.Add(this.rtb1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(303, 340);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(598, 215);
+            this.panel3.TabIndex = 2;
+            // 
+            // rtb3
+            // 
+            this.rtb3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.rtb3.Location = new System.Drawing.Point(810, 0);
+            this.rtb3.Name = "rtb3";
+            this.rtb3.Size = new System.Drawing.Size(405, 215);
+            this.rtb3.TabIndex = 2;
+            this.rtb3.Text = "";
+            // 
+            // rtb2
+            // 
+            this.rtb2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.rtb2.Location = new System.Drawing.Point(405, 0);
+            this.rtb2.Name = "rtb2";
+            this.rtb2.Size = new System.Drawing.Size(405, 215);
+            this.rtb2.TabIndex = 1;
+            this.rtb2.Text = "";
+            // 
+            // rtb1
+            // 
+            this.rtb1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.rtb1.Location = new System.Drawing.Point(0, 0);
+            this.rtb1.Name = "rtb1";
+            this.rtb1.Size = new System.Drawing.Size(405, 215);
+            this.rtb1.TabIndex = 0;
+            this.rtb1.Text = "";
+            // 
+            // panel2
+            // 
+            this.panel2.AllowDrop = true;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.panel2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.panel2.Location = new System.Drawing.Point(303, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.panel2.Size = new System.Drawing.Size(598, 340);
+            this.panel2.TabIndex = 3;
+            this.panel2.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel2_DragDrop);
+            this.panel2.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel2_DragEnter);
+            // 
+            // finish
+            // 
+            this.finish.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.finish.Location = new System.Drawing.Point(23, 340);
+            this.finish.Name = "finish";
+            this.finish.Size = new System.Drawing.Size(104, 30);
+            this.finish.TabIndex = 9;
+            this.finish.Text = "Finish";
+            this.finish.UseVisualStyleBackColor = true;
+            this.finish.Click += new System.EventHandler(this.finish_Click);
+            // 
+            // htmlFormBuilder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(901, 555);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Name = "htmlFormBuilder";
+            this.Text = "htmlFormBuilder";
+            this.Load += new System.EventHandler(this.htmlFormBuilder_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label inpTxt;
+        private System.Windows.Forms.Label inpEmail;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.RichTextBox rtb1;
+        private System.Windows.Forms.RichTextBox rtb3;
+        private System.Windows.Forms.RichTextBox rtb2;
+        private System.Windows.Forms.Label inpAdress;
+        private System.Windows.Forms.Label mobileNo;
+        private System.Windows.Forms.Label numberRange;
+        private System.Windows.Forms.Label radio;
+        private System.Windows.Forms.Label check;
+        private System.Windows.Forms.FlowLayoutPanel panel2;
+        private System.Windows.Forms.Label textarea;
+        private System.Windows.Forms.Label select;
+        private System.Windows.Forms.Button finish;
+    }
+}
---
+++ WebEdit/htmlFormBuilder.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MsgBox;
+
+namespace htmlEditor
+{
+    public partial class htmlFormBuilder : Form
+    {
+        int yPos = 0;
+        int formNo = 1;
+        int[] startRange;
+        int[] endRange;
+        Dictionary<int, string> formList = new Dictionary<int, string>();
+        Dictionary<string, int> inpType = new Dictionary<string, int>();
+        int i = -1;
+        int j = 1;
+        int k = 1;
+        int l = 1;
+        int flNum = 0;
+        public htmlFormBuilder()
+        {
+            InitializeComponent();
+        }
+
+        #region Mouse Down
+        private void inpTxt_MouseDown(object sender, MouseEventArgs e)
+        {
+            inpTxt.DoDragDrop("Input Text", DragDropEffects.Move);
+        }
+
+        private void inpEmail_MouseDown(object sender, MouseEventArgs e)
+        {
+            inpEmail.DoDragDrop("Input E-mail", DragDropEffects.Move);
+        }
+
+        private void inpAdress_MouseDown(object sender, MouseEventArgs e)
+        {
+            inpAdress.DoDragDrop("Input Address", DragDropEffects.Move);
+        }
+
+        private void mobileNo_MouseDown(object sender, MouseEventArgs e)
+        {
+            mobileNo.DoDragDrop("Number", DragDropEffects.Move);
+        }
+
+        private void numberRange_MouseDown(object sender, MouseEventArgs e)
+        {
+            numberRange.DoDragDrop("Range", DragDropEffects.Move);
+        }
+
+        private void radio_MouseDown(object sender, MouseEventArgs e)
+        {
+            radio.DoDragDrop("Radio", DragDropEffects.Move);
+        }
+
+        private void check_MouseDown(object sender, MouseEventArgs e)
+        {
+            check.DoDragDrop("Check", DragDropEffects.Move);
+        }
+
+        private void select_MouseDown(object sender, MouseEventArgs e)
+        {
+            select.DoDragDrop("Select", DragDropEffects.Move);
+        }
+
+        private void textarea_MouseDown(object sender, MouseEventArgs e)
+        {
+            textarea.DoDragDrop("Textarea", DragDropEffects.Move);
+        }
+
+        #endregion
+
+        #region drag enter
+        private void panel2_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Move;
+        }
+
+        private void inpEmail_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Move;
+        }
+
+        #endregion
+
+        private void panel2_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.StringFormat))
+            {
+                string str = (string)e.Data.GetData(DataFormats.StringFormat);
+                formList[flNum] = str;
+                flNum++;
+                Point cursorPos = new Point(0, 20);
+
+                switch (str)
+                {  
+                    case "Input Text":
+                        formInput(cursorPos, "Input Box", false, false, false, "");
+                        break;
+                    case "Input E-mail":
+                        formInput(cursorPos, "E-mail Input Box", false, false, false, "");
+                        break;
+                    case "Input Address":
+                        formInput(cursorPos, "Address Input Box", true, false, false, "");
+                        break;
+                    case "Number":
+                        formInput(cursorPos, "Number Input Box", false, false, false, "");
+                        break;
+                    case "Range":
+                        formInput(cursorPos, "Number Range Input Box", false, true, false, "");
+                        break;
+                    case "Radio":
+                        formInput(cursorPos, "Radio Input", false, false, true, str);
+                        break;
+                    case "Check":
+                        formInput(cursorPos, "Check Box Input", false, false, true, str);
+                        break;
+                    case "Select":
+                        formInput(cursorPos, "Drop Down List", false, false, true, str);
+                        break;
+                    case "Textarea":
+                        formInput(cursorPos, "Multiline Input Box", false, false, false, "");
+                        break;
+                }
+
+            }
+        }
+        // shows users input based on whats been dragged on to the area
+        public void formInput(Point cursorPos, string labelText, bool readOnly, bool requireRange, bool option, string type)
+        {
+            if (requireRange == true)
+            {
+                //Start Number
+                while (true)
+                {
+                    InputBoxResult result = InputBox.Show("Type a start number", "Start Number", "0", null);
+                    if (result.OK)
+                    {
+                        try
+                        {
+                            startRange = new int[] { Convert.ToInt32(result.Text) };
+                            i++;
+                            break;
+                        }
+                        catch (Exception)
+                        {
+                            MessageBox.Show("Type a number", "Please Type a number");
+                        }
+                    }
+                }
+                //End Number
+                while (true)
+                {
+                    InputBoxResult result2 = InputBox.Show("Type an end number", "End Number", "0", null);
+                    if (result2.OK)
+                    {
+                        try
+                        {
+                            if (startRange[i] >= 0 && Convert.ToInt32(result2.Text) <= startRange[i])
+                            {
+                                MessageBox.Show("End Rnage cannot be smaller than start range", "Number is invalid");
+                            }
+                            else
+                            {
+                                endRange = new int[] { Convert.ToInt32(result2.Text) };
+                                break;
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            MessageBox.Show("Type a number", "Please Type a number");
+                        }
+                    }
+                }
+            }
+
+            if (option == true)
+            {
+                while (true)
+                {
+                    InputBoxResult result3 = InputBox.Show("Type the number of elements for the radio, select or checkbox ect.", "Type the Number", "0", null);
+                    if (result3.OK && result3.Text != "0")
+                    {
+                        try
+                        {
+                            if (type == "Radio")
+                            {
+                                inpType[type + j] = Convert.ToInt32(result3.Text);
+                                j++;
+                            }
+                            else if(type == "Select")
+                            {
+                                inpType[type + k] = Convert.ToInt32(result3.Text);
+                                k++;
+                            }
+                            else if (type == "Check")
+                            {
+                                inpType[type + l] = Convert.ToInt32(result3.Text);
+                                l++;
+                            }
+                            break;
+                        }
+                        catch (Exception)
+                        {
+                            MessageBox.Show("Type a number", "Please Type a number");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Type a number greater than 0", "Please Type a number");
+                    }
+                }
+            }
+
+            Label info = new Label();
+            info.Location = new Point(73, cursorPos.Y + yPos);
+            info.Size = new Size(120, 17);
+            info.Text = labelText;
+            info.Name = "info" + formNo;
+            panel2.Controls.Add(info);
+
+            TextBox txtBox = new TextBox();
+            txtBox.Location = new Point(cursorPos.Y, cursorPos.Y + yPos + 30);
+            txtBox.Size = new Size(187, 22);
+            txtBox.ReadOnly = readOnly;
+            txtBox.Name = "txtBoxN" + formNo;
+            if (readOnly == false)
+            {
+                txtBox.Text = "This is for the Name of the form element";
+            }
+            else
+            {
+                txtBox.Text = "Nothing to type here";
+            }
+            panel2.Controls.Add(txtBox);
+
+            TextBox txtBox1 = new TextBox();
+            txtBox1.Location = new Point(cursorPos.Y, cursorPos.Y + yPos + 60);
+            txtBox1.Size = new Size(187, 22);
+            txtBox1.ReadOnly = readOnly;
+            txtBox1.Name = "txtBoxP" + formNo;
+            if (readOnly == false)
+            {
+                if (type == "Radio" || type == "Check" || type == "Select")
+                {
+                    txtBox1.Text = "Nothing to type here";
+                    txtBox1.ReadOnly = true;
+                }
+                else
+                {
+                    txtBox1.Text = "Type placeholder text in here";
+                }
+            }
+            else
+            {
+                txtBox1.Text = "Nothing to type here";
+            }
+            panel2.Controls.Add(txtBox1);
+
+            yPos = yPos + cursorPos.Y + 90;
+            formNo++;
+
+        }
+
+        private void htmlFormBuilder_Load(object sender, EventArgs e)
+        {
+            panel2.AllowDrop = true;
+            WindowState = FormWindowState.Maximized;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void finish_Click(object sender, EventArgs e)
+        {
+            Dictionary<int, string> htmlText = new Dictionary<int, string>();
+            TextBox txtbxN1;
+            int num = -2;
+            int num2 = 0;
+            int m = 0;
+            if (yPos == 0)
+            {
+                MessageBox.Show("Please Choose form elements before finsishing", "Choose some elements");
+            }
+            else
+            {
+                for (int i = 0; i < formList.Count; i++)
+                {
+                    switch (formList[i])
+                    {
+                        case "Input Text":
+                            num = num + 3;
+                            num2 = num2 + 2;
+                            htmlText[i] = @" " + panel2.Controls[num].Text + @": <input type='text' name='" + panel2.Controls[num].Text + @"' placeholder='"+ panel2.Controls[num2].Text + @"'><br>";
+                            rtb1.Text += htmlText[i] + "\n";
+                            break;
+                        case "Input E-mail":
+                            num = num + 3;
+                            num2 = num2 + 3;
+                            htmlText[i] = @"  Email: <input type='email' name='" + panel2.Controls[num].Text + @"' placeholder='" + panel2.Controls[num2].Text + @"'><br>";
+                            rtb1.Text += htmlText[i];
+                            break;
+                        case "Input Address":
+                            num = num + 3;
+                            num2 = num2 + 2;
+                            htmlText[i] = @"";
+                            rtb1.Text += htmlText[i] + "\n";
+                            break;
+                        case "Number":
+                            num = num + 3;
+                            num2 = num2 + 2;
+                            htmlText[i] = @" " + panel2.Controls[num].Text + @": <input type='number' name='" + panel2.Controls[num].Text + @"' placeholder='" + panel2.Controls[num2].Text + @"'><br>";
+                            rtb1.Text += htmlText[i] + "\n";
+                            break;
+                        case "Range":
+                            num = num + 3;
+                            num2 = num2 + 2;
+                            htmlText[i] = @" " + panel2.Controls[num].Text + @": <input type='number' min='" + startRange[m] + "' max='" + endRange[m] + "' name='" + panel2.Controls[num].Text + @"' placeholder='" + panel2.Controls[num2].Text + @"'><br>";
+                            rtb1.Text += htmlText[i] + "\n";
+                            m++;
+                            break;
+                        case "Radio":
+                            num = num + 3;
+                            num2 = num2 + 2;
+                            htmlText[i] = @"<input type='radio' name='gender' value='" + panel2.Controls[num2].Text + "'> Male<br>";
+                            rtb1.Text += htmlText[i] + "\n";
+                            break;
+                        case "Check":
+                            num = num + 3;
+                            num2 = num2 + 2;
+                            htmlText[i] = @"";
+                            rtb1.Text += htmlText[i] + "\n";
+                            break;
+                        case "Select":
+                            num = num + 3;
+                            num2 = num2 + 2;
+                            htmlText[i] = @"";
+                            rtb1.Text += htmlText[i] + "\n";
+                            break;
+                        case "Textarea":
+                            num = num + 3;
+                            num2 = num2 + 2;
+                            htmlText[i] = @" " + panel2.Controls[num].Text + @": <textarea name='" + panel2.Controls[num].Text + @"' rows='10' cols='30'></textarea><br>";
+                            rtb1.Text += htmlText[i] + "\n";
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
